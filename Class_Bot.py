97758a769493b4138f41ad601b55053be89f795b
Therealcorwin <therealcorwin@gmail.com>
2022-07-14 02:05:16 +0200

    Correction Sonarcloud

--- Class_Bot.py
+++ Class_Bot.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 @dataclass
-class message_utilisateur_private:
+class MessageUtilisateurPrivate:
     message_text: str
     message_type: str
     message_id: int
@@ -15,19 +15,19 @@ class message_utilisateur_private:
     def __post_init__(self):
         self.message_full_authorname = f"{self.message_author_first_name} {self.message_author_last_name}"
         """
-            print("message_utilisateur_private message_text : ", self.message_text)
-            print("message_utilisateur_private message_type : ", self.message_type)
-            print("message_utilisateur_private message_id : ", self.message_id)
-            print("message_utilisateur_private message_date : ", self.message_date)
-            print("message_utilisateur_private message_author_id : ", self.message_author_id)
-            print("message_utilisateur_private message_author_signature : ", self.message_author_first_name)
-            print("message_utilisateur_private message_author_last_name : ", self.message_author_last_name)
-            print("message_utilisateur_private message_full_authorname : ", self.message_full_authorname)
+            print("MessageUtilisateurPrivate message_text : ", self.message_text)
+            print("MessageUtilisateurPrivate message_type : ", self.message_type)
+            print("MessageUtilisateurPrivate message_id : ", self.message_id)
+            print("MessageUtilisateurPrivate message_date : ", self.message_date)
+            print("MessageUtilisateurPrivate message_author_id : ", self.message_author_id)
+            print("MessageUtilisateurPrivate message_author_signature : ", self.message_author_first_name)
+            print("MessageUtilisateurPrivate message_author_last_name : ", self.message_author_last_name)
+            print("MessageUtilisateurPrivate message_full_authorname : ", self.message_full_authorname)
         """
         
 
 @dataclass
-class message_utilisateur_channel:
+class MessageUtilisateurChannel:
     message_text: str
     message_type: str
     message_id: int
@@ -35,15 +35,17 @@ class message_utilisateur_channel:
     message_from_chat: str
     message_from_chat_id: int
     message_full_authorname: str = "None"
-    
+
+    def __post_init__(self):    
+        print("MessageUtilisateurChannel message_id : ", self.message_id)
+
     """
-    def __post_init__(self):
-        print("message_utilisateur_channel message_text :", self.message_text)
-        print("message_utilisateur_channel message_type : ", self.message_type)
-        print("message_utilisateur_channel message_id : ", self.message_id)
-        print("message_utilisateur_channel message_date : ", self.message_date)
-        print("message_utilisateur_channel message_from_chat : ", self.message_from_chat)
-        print("message_utilisateur_channel message_from_chat_id : ", self.message_from_chat_id)
-        print("message_utilisateur_channel message_full_authorname : ", self.message_full_authorname)
+        print("MessageUtilisateurChannel message_text :", self.message_text)
+        print("MessageUtilisateurChannel message_type : ", self.message_type)
+        print("MessageUtilisateurChannel message_id : ", self.message_id)
+        print("MessageUtilisateurChannel message_date : ", self.message_date)
+        print("MessageUtilisateurChannel message_from_chat : ", self.message_from_chat)
+        print("MessageUtilisateurChannel message_from_chat_id : ", self.message_from_chat_id)
+        print("MessageUtilisateurChannel message_full_authorname : ", self.message_full_authorname)
     """
     
