9a70444b253d838461ce4f351cf8a5f288852582
therealcorwin <5471363+therealcorwin@users.noreply.github.com>
2024-04-28 16:10:46 +0200

    improve

--- Class_Bot.py
+++ Class_Bot.py
@@ -7,24 +7,21 @@ class MessageUtilisateurPrivate:
     message_id: int
     message_date: int
     message_author_id: int
-    message_telegram_user_name: str = "None"
-    message_author_first_name: str = "None"
-    message_author_last_name: str = "None"
-    message_full_authorname: str = "None"
- 
+
     def __post_init__(self):
         self.message_full_authorname = f"{self.message_author_first_name} {self.message_author_last_name}"
-        """
-            print("MessageUtilisateurPrivate message_text : ", self.message_text)
-            print("MessageUtilisateurPrivate message_type : ", self.message_type)
-            print("MessageUtilisateurPrivate message_id : ", self.message_id)
-            print("MessageUtilisateurPrivate message_date : ", self.message_date)
-            print("MessageUtilisateurPrivate message_author_id : ", self.message_author_id)
-            print("MessageUtilisateurPrivate message_author_signature : ", self.message_author_first_name)
-            print("MessageUtilisateurPrivate message_author_last_name : ", self.message_author_last_name)
-            print("MessageUtilisateurPrivate message_full_authorname : ", self.message_full_authorname)
-        """
-        
+
+    @property
+    def message_author_first_name(self) -> str:
+        return "None"
+
+    @property
+    def message_author_last_name(self) -> str:
+        return "None"
+
+    @property
+    def message_telegram_user_name(self) -> str:
+        return "None"
 
 @dataclass
 class MessageUtilisateurChannel:
@@ -36,16 +33,19 @@ class MessageUtilisateurChannel:
     message_from_chat_id: int
     message_full_authorname: str = "None"
 
-    def __post_init__(self):    
+    def __post_init__(self):
         print("MessageUtilisateurChannel message_id : ", self.message_id)
 
-    """
-        print("MessageUtilisateurChannel message_text :", self.message_text)
-        print("MessageUtilisateurChannel message_type : ", self.message_type)
-        print("MessageUtilisateurChannel message_id : ", self.message_id)
-        print("MessageUtilisateurChannel message_date : ", self.message_date)
-        print("MessageUtilisateurChannel message_from_chat : ", self.message_from_chat)
-        print("MessageUtilisateurChannel message_from_chat_id : ", self.message_from_chat_id)
-        print("MessageUtilisateurChannel message_full_authorname : ", self.message_full_authorname)
-    """
-    
+    def _from_private_(self, private: MessageUtilisateurPrivate) -> None:
+        self.message_text = private.message_text
+        self.message_type = private.message_type
+        self.message_id = private.message_id
+        self.message_date = private.message_date
+        self.message_full_authorname = private.message_full_authorname
+        self.message_from_chat = private.message_author_first_name
+        self.message_from_chat_id = private.message_author_id
+
+
+private = MessageUtilisateurPrivate(message_text="Hello, world!", message_type="text", message_id=1234, message_date=1643218832, message_author_id=5678)
+channel = MessageUtilisateurChannel(message_text="", message_type="", message_id=0, message_date=0, message_from_chat="", message_from_chat_id=0)
+channel._from_private_(private)
